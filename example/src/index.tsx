ecbd3942406059ae21be9027ca96a11e5ac49049
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-03-06 13:57:48 -0300

    Add theme provider to example app

--- example/src/index.tsx
+++ example/src/index.tsx
@@ -1,10 +1,27 @@
+import { useState } from "react"
+import { useColorScheme } from "react-native"
 import { GestureHandlerRootView } from "react-native-gesture-handler"
+
+import { Settings, SettingsProvider } from "./services/settings"
+import { ThemeProvider } from "./theme"
 
 
 export function App() {
+
+
+    const colorScheme = useColorScheme()
+    const [settings, setSettings] = useState<Settings>({
+        isDark: colorScheme === "dark",
+    })
+
+
     return (
         <GestureHandlerRootView style={{ flex: 1 }}>
+            <SettingsProvider settings={settings} setSettings={setSettings}>
+                <ThemeProvider>
 
+                </ThemeProvider>
+            </SettingsProvider>
         </GestureHandlerRootView>
     )
 }
---
+++ example/src/theme/index.tsx
@@ -0,0 +1,28 @@
+import { PropsWithChildren } from "react"
+import {
+    MaterialDarkTheme,
+    MaterialLightTheme,
+    MaterialProvider,
+} from "react-material-design-provider"
+import { MD3DarkTheme, MD3LightTheme, PaperProvider } from "react-native-paper"
+
+import { useSettings } from "../services/settings"
+
+
+export function ThemeProvider({ children }: PropsWithChildren) {
+
+
+    const { settings } = useSettings()
+
+    const paperTheme = settings.isDark ? MD3DarkTheme : MD3LightTheme
+    const materialTheme = settings.isDark ? MaterialDarkTheme : MaterialLightTheme
+
+
+    return (
+        <PaperProvider theme={paperTheme}>
+            <MaterialProvider theme={materialTheme}>
+                {children}
+            </MaterialProvider>
+        </PaperProvider>
+    )
+}
