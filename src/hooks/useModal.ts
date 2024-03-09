a94b5d76c811be654ed82462a20fea25d1747011
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-03-09 17:32:45 -0300

    Add useModal hook

---
+++ src/hooks/useModal.ts
@@ -0,0 +1,22 @@
+import { useState } from "react"
+
+
+export type UseModal = {
+    isVisible: boolean
+    show: () => void
+    hide: () => void
+}
+
+
+export function useModal(isVisible: boolean = false): UseModal {
+
+
+    const [isOpen, setIsOpen] = useState(isVisible)
+
+
+    return {
+        isVisible: isOpen,
+        show: () => setIsOpen(true),
+        hide: () => setIsOpen(false),
+    }
+}
