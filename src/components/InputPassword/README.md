97834b622f3084c735a0f89289a59f8d885d5d44
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-03-11 12:54:33 -0300

    Add InputPassword component

---
+++ src/components/InputPassword/README.md
@@ -0,0 +1,7 @@
+# InputPassword
+
+Input component that is styled and configured to be used for password fields.
+
+## Usage
+
+Its usage is the same as `TextInput` from `react-native-paper`.
---
+++ src/components/InputPassword/index.tsx
@@ -0,0 +1,45 @@
+import { forwardRef, useState } from "react"
+import { KeyboardTypeOptions, TextInput as RNTextInput } from "react-native"
+import { TextInput } from "react-native-paper"
+
+import { Input, InputProps } from "../Input"
+
+
+export interface InputPasswordProps extends InputProps {}
+
+
+export const InputPassword = forwardRef<RNTextInput, InputPasswordProps>((props, ref) => {
+
+
+    const [hidePassword, setHidePassword] = useState(true)
+
+    const isPasswordHidden: boolean = props.secureTextEntry === undefined
+        ? hidePassword
+        : props.secureTextEntry
+    const keyboardType: KeyboardTypeOptions = props.keyboardType === undefined
+        ? isPasswordHidden ? "default" : "visible-password"
+        : props.keyboardType
+
+
+    function right() {
+        return <TextInput.Icon
+            icon={isPasswordHidden ? "eye-off" : "eye"}
+            onPress={() => setHidePassword(!isPasswordHidden)}
+            forceTextInputFocus={false}
+        />
+    }
+
+
+    return (
+        <Input
+            ref={ref}
+            autoCapitalize={"none"}
+            autoComplete={"new-password"}
+            autoCorrect={false}
+            right={props.right === undefined ? right() : props.right}
+            {...props}
+            secureTextEntry={isPasswordHidden}
+            keyboardType={keyboardType}
+        />
+    )
+})
