da86c1a81aea5701d863ab11d05a67321e6ec4ef
viniciusyoshioka <71671043+viniciusyoshioka@users.noreply.github.com>
2024-03-22 17:05:30 -0300

    chore(example): Update eslint config according to new version

--- example/.eslintrc.js
+++ example/.eslintrc.js
@@ -2,4 +2,8 @@
 module.exports = {
     root: true,
     extends: "@vinicius1313/eslint-config",
+    parserOptions: {
+        project: "tsconfig.eslint.json",
+        tsconfigRootDir: __dirname,
+    },
 }
